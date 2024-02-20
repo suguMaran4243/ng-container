6a5ea174bbc9ceef0cb8aa40599d3c60924f2210
suguMaranram <sugumaran.ram@aspiresys.com>
2024-02-20 14:06:10 +0530

    container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-container';
+  toggle:boolean=true;
+  toToggle()
+  {
+     this.toggle=!this.toggle;
+  }
 }
